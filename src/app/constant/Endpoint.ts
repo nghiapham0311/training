3c8bdc9162ddc1a25c5c2c343db75d3bb8a8e807
Pham Huu Nghia <Nghia.PhamHuu@vn.bosch.com>
2024-10-06 10:13:10 +0700

    update

--- src/app/constant/Endpoint.ts
+++ src/app/constant/Endpoint.ts
@@ -1,7 +1,7 @@
 import { environment } from '../../environments/environment';
 
-const GATEWAY_ENDPOINT = environment.gatewayService;
-const FUNC_ENDPOINT = environment.funcService;
+const GATEWAY_ENDPOINT = "https://topx-app-gateway-dev-southeastasia-001.azurewebsites.net";
+const FUNC_ENDPOINT = "https://topx-func-file-dev-southeastasia-001.azurewebsites.net";
 
 export const ACCOUNTS_ENDPOINT = {
   MEMBER_ME: `${GATEWAY_ENDPOINT}/accounts/member/me`,
